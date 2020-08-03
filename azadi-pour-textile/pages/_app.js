f7fc63f5b99b342264558526884d2dc300138fe3
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-03 23:07:10 +0430

    create custom app container

---
+++ azadi-pour-textile/pages/_app.js
@@ -0,0 +1,44 @@
+import App from "next/app";
+import Cookie from 'js-cookie';
+
+import azadiPourTextile from '../api/azadiPourTextile';
+
+class _App extends App {
+    static async getInitialProps({Component, router, ctx}) {
+        let pageProps = {};
+        const auth = {};
+
+        const token = ((ctx.req || {}).cookies || {}).jwt || Cookie.get('jwtClient');
+
+        try {
+            if (!token) {
+                throw new Error();
+            }
+            const userRes = await azadiPourTextile.post('/users/auth/checktoken', {}, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            auth.isSignedIn = true;
+            auth.user = userRes.data.data.user;
+        } catch (err) {
+            auth.isSignedIn = false;
+        }
+
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx, auth);
+        }
+
+        return {pageProps, auth};
+    }
+
+    render() {
+        const {Component, pageProps, auth} = this.props;
+
+        return (
+            <Component {...pageProps} auth={auth}/>
+        );
+    }
+};
+
+export default _App;
