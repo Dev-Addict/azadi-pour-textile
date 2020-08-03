635091b5444d3262edb48504a07999cd6e69da41
WolfMAN <aria.azadi.pour@protonmail.com>
2020-08-03 21:38:18 +0430

    create user router

--- azadi-pour-textile/server/routes/APIRouter.js
+++ azadi-pour-textile/server/routes/APIRouter.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const userRouter = require('./userRouter');
+
 const router = express.Router({mergeParams: true});
+
+router.route('/users', userRouter);
 
 module.exports = router;
